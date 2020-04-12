3ced1e4d84463d1a3f328bbef45a15c3e1667e86
Steven Smith <steven.76484@gmail.com>
2020-04-12 17:01:33 -0400

    Adding POST handle to /books endpoint to create new book

--- app.js
+++ app.js
@@ -12,21 +12,28 @@ mongoose.connect('mongodb://localhost/bookAPI', {
   useUnifiedTopology: true,
 });
 
-bookRouter.route('/books').get((req, res) => {
-  const query = {};
+bookRouter
+  .route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+    book.save();
+    return res.status(201).json(book);
+  })
+  .get((req, res) => {
+    const query = {};
 
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
+    if (req.query.genre) {
+      query.genre = req.query.genre;
     }
 
-    return res.json(books);
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+
+      return res.json(books);
+    });
   });
-});
 
 bookRouter.route('/books/:bookId').get((req, res) => {
   Book.findById(req.params.bookId, (err, book) => {
