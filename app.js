54fb2729f9a865e55d1a84c2e4e4344d9c585133
Steven Smith <steven.76484@gmail.com>
2020-04-12 17:06:58 -0400

    Refactoring bookRouter into separate file

--- app.js
+++ app.js
@@ -2,46 +2,15 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
+
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = express.Router();
-const Book = require('./models/bookModel');
 
 mongoose.connect('mongodb://localhost/bookAPI', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-bookRouter
-  .route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-
-      return res.json(books);
-    });
-  });
-
-bookRouter.route('/books/:bookId').get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(book);
-  });
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
